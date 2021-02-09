9c17084c16ce1227bf247ee4e73fe54230bfc217
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-02-09 11:16:55 -0500

    Se crea una base activity (BaseActivity.java)

---
+++ app/src/main/java/com/example/phenlineaapp/Base/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.example.phenlineaapp.Base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BaseActivity extends AppCompatActivity {
+
+    public static final String LOG = "log_data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void createToast(Context context, String message, int duration){
+        Toast.makeText(context, message, duration).show();
+    }
+
+    protected void setLog(String message){
+        Log.i(LOG, message);
+    }
+
+    protected void goToHome(Context context){
+        //startActivity(new Intent(context, HomeActivity.class));
+        createToast(getApplicationContext(), "going to home", Toast.LENGTH_SHORT);
+    }
+}
