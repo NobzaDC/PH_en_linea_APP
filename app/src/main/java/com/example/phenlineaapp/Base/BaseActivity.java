0c64841fcd00e76f789a9f82c5f90e3b8d27d17b
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-02-22 14:49:50 -0500

    Se agrega la funcion BaseActivity.getPref para obtener los datos de SharedPreference

--- app/src/main/java/com/example/phenlineaapp/Base/BaseActivity.java
+++ app/src/main/java/com/example/phenlineaapp/Base/BaseActivity.java
@@ -1,18 +1,20 @@
 package com.example.phenlineaapp.Base;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.phenlineaapp.Views.Activitys.HomeActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
     public static final String LOG = "log_data";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +25,15 @@ public class BaseActivity extends AppCompatActivity {
         Toast.makeText(context, message, duration).show();
     }
 
-    protected void setLog(String message){
+    protected void setLog(String message) {
         Log.i(LOG, message);
     }
 
-    protected void goToHome(Context context){
+    protected void goToHome(Context context) {
         startActivity(new Intent(context, HomeActivity.class));
+    }
+
+    public SharedPreferences getPref() {
+        return getSharedPreferences("PhenlineaPreferences", MODE_PRIVATE);
     }
 }
