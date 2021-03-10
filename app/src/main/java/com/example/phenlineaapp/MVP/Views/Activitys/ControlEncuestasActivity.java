cfc2361b0c7164539c02deef1f08f82f169bab1e
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-03-10 12:45:44 -0500

    Se crean activitys para cada uno de los botones de la app

---
+++ app/src/main/java/com/example/phenlineaapp/MVP/Views/Activitys/ControlEncuestasActivity.java
@@ -0,0 +1,16 @@
+package com.example.phenlineaapp.MVP.Views.Activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.phenlineaapp.R;
+
+public class ControlEncuestasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_encuestas);
+    }
+}
---
+++ app/src/main/java/com/example/phenlineaapp/MVP/Views/Activitys/ControlFacturasActivity.java
@@ -0,0 +1,16 @@
+package com.example.phenlineaapp.MVP.Views.Activitys;
+
+
+import android.os.Bundle;
+
+import com.example.phenlineaapp.Base.Activitys.BaseActivity;
+import com.example.phenlineaapp.R;
+
+public class ControlFacturasActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_facturas);
+    }
+}
