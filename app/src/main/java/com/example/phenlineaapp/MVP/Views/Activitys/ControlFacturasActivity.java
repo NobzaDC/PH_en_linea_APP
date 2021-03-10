a51c5c88cb9c97c9285935d8595d11a111c300ca
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-03-10 17:01:00 -0500

    Se crea la vista de ControlFacturasActivity.java y se implementa su respectivo binding

--- app/src/main/java/com/example/phenlineaapp/MVP/Views/Activitys/ControlFacturasActivity.java
+++ app/src/main/java/com/example/phenlineaapp/MVP/Views/Activitys/ControlFacturasActivity.java
@@ -4,13 +4,16 @@ package com.example.phenlineaapp.MVP.Views.Activitys;
 import android.os.Bundle;
 
 import com.example.phenlineaapp.Base.Activitys.BaseActivity;
-import com.example.phenlineaapp.R;
+import com.example.phenlineaapp.databinding.ActivityControlFacturasBinding;
 
 public class ControlFacturasActivity extends BaseActivity {
+
+    private ActivityControlFacturasBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_control_facturas);
+        binding = ActivityControlFacturasBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
