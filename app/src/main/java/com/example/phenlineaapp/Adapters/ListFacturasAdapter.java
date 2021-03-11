93f89b085f564d40fb9f40e1fdc4f4b0673d3c96
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-03-11 12:18:11 -0500

    Se crea ListFacturasAdapter y ListFacturasModel para utilizarlos en la lista de facturas, ademas se modifica list_peridos_facturas para agregarle un nuevo campo

---
+++ app/src/main/java/com/example/phenlineaapp/Adapters/ListFacturasAdapter.java
@@ -0,0 +1,55 @@
+package com.example.phenlineaapp.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.phenlineaapp.Models.ListFacturasModel;
+import com.example.phenlineaapp.R;
+
+import java.util.List;
+
+public class ListFacturasAdapter extends RecyclerView.Adapter<ListFacturasAdapter.ViewHolderFacturas> {
+
+    private final List<ListFacturasModel> mList;
+
+    public ListFacturasAdapter(List<ListFacturasModel> mList) {
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public ListFacturasAdapter.ViewHolderFacturas onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_periodos_facturas, null, false);
+        return new ViewHolderFacturas(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListFacturasAdapter.ViewHolderFacturas holder, int position) {
+        holder.tvAño.setText(mList.get(position).getAnio());
+        holder.tvMes.setText(mList.get(position).getMes());
+        holder.tvPeriodo.setText(mList.get(position).getPeriodo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class ViewHolderFacturas extends RecyclerView.ViewHolder {
+
+        TextView tvAño, tvMes, tvPeriodo;
+
+        public ViewHolderFacturas(@NonNull View itemView) {
+            super(itemView);
+            tvAño = itemView.findViewById(R.id.tv_facturas_año);
+            tvMes = itemView.findViewById(R.id.tv_facturas_mes);
+            tvPeriodo = itemView.findViewById(R.id.tv_facturas_periodo);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/phenlineaapp/Models/ListFacturasModel.java
@@ -0,0 +1,38 @@
+package com.example.phenlineaapp.Models;
+
+public class ListFacturasModel {
+
+    private String anio;
+    private String mes;
+    private String periodo;
+
+    public ListFacturasModel(String anio, String mes, String periodo) {
+        this.anio = anio;
+        this.mes = mes;
+        this.periodo = periodo;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+}
