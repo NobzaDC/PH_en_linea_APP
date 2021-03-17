91e9d2d9ef0e8bb56f12705e36074a5309488d24
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-03-17 14:52:13 -0500

    Se agrega onclick al header del drawer layout

--- app/src/main/java/com/example/phenlineaapp/MVP/Views/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/phenlineaapp/MVP/Views/Fragments/HomeFragment.java
@@ -1,21 +1,15 @@
 package com.example.phenlineaapp.MVP.Views.Fragments;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.phenlineaapp.Adapters.ViewPagerAdapter;
-import com.example.phenlineaapp.MVP.Interfaces.Methods.MethodCall;
-import com.example.phenlineaapp.Methods.Calls;
-import com.example.phenlineaapp.R;
 import com.example.phenlineaapp.databinding.FragmentHomeBinding;
 
 import org.jetbrains.annotations.NotNull;
@@ -23,8 +17,6 @@ import org.jetbrains.annotations.NotNull;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private Dialog dialog;
-    private MethodCall methodCall;
 
     public HomeFragment() {
     }
@@ -34,7 +26,6 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         binding.tabLayout.setupWithViewPager(binding.viewPager);
-        methodCall = new Calls(getContext(), getActivity());
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
         binding.viewPager.setAdapter(viewPagerAdapter);
         binding.viewPager.setSaveEnabled(false);
@@ -45,29 +36,6 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        dialog = new Dialog(getContext());
-        dialog.setContentView(R.layout.detail_cooproperty_dialog);
-        dialog.getWindow().setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.background_detail_cooproperty));
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().getAttributes().windowAnimations = R.style.dialog_animation;
-        Button btnCallAdmin = dialog.findViewById(R.id.btnCallAdmin);
-        btnCallAdmin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                methodCall.call("3006922336");
-                dialog.dismiss();
-            }
-        });
-
-        binding.btnSeeDetails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.show();
-            }
-        });
-    }
-
-    private void callAdmin(String tel) {
 
     }
 }
