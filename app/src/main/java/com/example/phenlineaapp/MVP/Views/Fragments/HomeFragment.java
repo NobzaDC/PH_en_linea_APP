ac3c213d56f74b62fbfe4438d03b3faf131a778c
Pablo Gonzalez <gonzalezp0813@gmail.com>
2021-03-18 09:34:51 -0500

    Se cambia le layout, se agregan iconos y se cambia a un mejor responsive

--- app/src/main/java/com/example/phenlineaapp/MVP/Views/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/phenlineaapp/MVP/Views/Fragments/HomeFragment.java
@@ -7,14 +7,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import com.example.phenlineaapp.Adapters.ViewPagerAdapter;
+import com.example.phenlineaapp.Base.Fragments.BaseFragment;
+import com.example.phenlineaapp.R;
 import com.example.phenlineaapp.databinding.FragmentHomeBinding;
 
 import org.jetbrains.annotations.NotNull;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends BaseFragment {
 
     private FragmentHomeBinding binding;
 
@@ -25,11 +26,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        init();
+        return binding.getRoot();
+    }
+
+    private void init() {
         binding.tabLayout.setupWithViewPager(binding.viewPager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
         binding.viewPager.setAdapter(viewPagerAdapter);
         binding.viewPager.setSaveEnabled(false);
-        return binding.getRoot();
+        binding.tabLayout.getTabAt(0).setIcon(R.drawable.ic_economy);
+        binding.tabLayout.getTabAt(1).setIcon(R.drawable.ic_informes);
+        binding.tabLayout.getTabAt(2).setIcon(R.drawable.ic_contact);
+        binding.tabLayout.getTabAt(3).setIcon(R.drawable.ic_social);
     }
 
     @Override
